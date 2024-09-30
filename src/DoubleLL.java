d5f0c5f3da2d00dcb8f3cacdd4a9f90a4d2916cb
Miles Padot <mpadot@emich.edu>
2024-09-29 21:44:29 -0400

    Initial commit

--- src/DoubleLL.java
+++ src/DoubleLL.java
@@ -1,4 +1,85 @@
-package PACKAGE_NAME;
-
 public class DoubleLL {
+    Node head, tail;
+
+    class Node {
+        int data;
+        Node prev;
+        Node next;
+
+        public Node(int data) {
+
+            this.data = data;
+        }
+    }
+
+
+    public void remove(Node n) {
+        if (n == null)
+            return;
+
+
+        if (n == head) {
+            head = n.next;
+            if (head != null) {
+                head.prev = null;
+            } else {
+                tail = null;
+            }
+        }
+
+        else if (n == tail) {
+            tail = n.prev;
+            if (tail != null) {
+                tail.next = null;
+            } else {
+                head = null;
+            }
+        }
+
+        else if (n.prev != null && n.next != null) {
+            n.prev.next = n.next;
+            n.next.prev = n.prev;
+        }
+    }
+
+    public void insert(Node n) {
+        if (head == null) {
+            head = n;
+            tail = n;
+            return;
+        }
+        tail.next = n;
+        n.prev = tail;
+        tail = n;
+    }
+
+    public void print() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " --> ");
+            current = current.next;
+        }
+        System.out.println();
+
+    }
+
+
+    public void deleteSecondToLast() {
+        if (head == null || head.next == null) {
+
+            return;
+        }
+
+
+        if (head.next == tail) {
+            remove(head);
+            return;
+        }
+        Node current = tail.prev;
+
+        if (current != null) {
+            remove(current);
+        }
+    }
 }
+
--- src/Driver.java
+++ src/Driver.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class Driver {
+    public static void main(String[] args) {
+        DoubleLL lst = new DoubleLL();
+        DoubleLL.Node nine  = lst.new Node(9);
+        lst.insert(lst.new Node (7));
+        lst.insert(lst.new Node (3));
+        lst.insert(lst.new Node (5000));
+        lst.insert(lst.new Node (10));
+        lst.insert(lst.new Node (30));
+
+        lst.insert(nine);
+        lst.print();
+        //lst.insert(lst.new Node (4));
+        lst.deleteSecondToLast();
+        lst.print();
+
+
+    }
 }
+
